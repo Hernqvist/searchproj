9f85fc3ad0bad726d6211ca23db90047c2403f04
Eliisabet Hein <s1447681@sms.ed.ac.uk>
2019-05-14 10:00:09 +0200

    created database, added some safety checks to test script

--- run_tests.py
+++ run_tests.py
@@ -2,12 +2,19 @@ import common
 import subprocess
 import os
 from pydub import AudioSegment
+from pydub.exceptions import CouldntDecodeError
 
 correct = [[0 for density in common.DENSITY_RANGE] for bits in common.BIT_RANGE]
 numfiles = 0
 for filename in os.listdir("songs"):
+  if (filename == ".DS_Store") or (filename == ".DSStore"):
+    continue
   numfiles += 1
-  song = AudioSegment.from_mp3("songs/"+filename)
+  try:
+    song = AudioSegment.from_mp3("songs/"+filename)
+  except CouldntDecodeError:
+    print("Failed to decode " + filename + " as an audio file, skipped")
+    continue
   sample = song[:20000][-5000:]
   sample.export("sample.mp3", format="mp3")
   for i, bits in enumerate(common.BIT_RANGE):
@@ -19,8 +26,8 @@ for filename in os.listdir("songs"):
       print(filename, bits, "bits,", density, "density", match)
       if match:
         correct[i][j] += 1
-
-
+  if numfiles == 25:
+    break
 
 print("\t"+"".join(["d:{}\t".format(d) for d in common.DENSITY_RANGE]))
 for i, bits in enumerate(common.BIT_RANGE):
