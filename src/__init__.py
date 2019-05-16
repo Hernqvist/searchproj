42eb606b9e59b3c7f4dcaa05aacd0fb3774f07c4
mthormati <mthormati@gmail.com>
2019-05-16 23:50:59 +0200

    Add timer for audio recording

--- src/__init__.py
+++ src/__init__.py
@@ -7,24 +7,29 @@ import subprocess
 
 app = Flask(__name__)
 
+# Home route
 @app.route('/')
 def index():
     return render_template('audio.html')
 
+# Route for uploading and analyzing audio
 @app.route('/audio-upload', methods=['POST'])
 def audio_upload():
+    # Read audio file
     audioContent = request.files['audio_data'].read()
     webmAudio = AudioSegment.from_file(io.BytesIO(audioContent), format='webm', codec='opus')
-
+    # Check if data has been indexed
     fileName = './audioFiles/audio-regular.mp3'
     if not os.path.exists('./audioFiles/'):
         os.makedirs('./audioFiles')
     if not os.path.exists('./databases/database.pklz'):
         return 'Dataset has not been indexed;;'
+    # Export audio
     webmAudio.export(fileName, format="mp3")
 
     tryOtherPitches = False
 
+    # Delete sgram files
     sgramFile = ''
     for filename in os.listdir('./src/static'):
         if filename.startswith("sgram"): 
@@ -39,11 +44,14 @@ def audio_upload():
             else:
                 return output + ';' + sgramFile
         else:
+            # Run fingerprinting algorithm
             output = str(subprocess.check_output(['python3', 'audfprint.py', 'match', '--dbase', 'databases/database.pklz', fileName, '--illustrate']))
+            # Create sgram image file
             for filename in os.listdir('./src/static'):
                 if filename.startswith("sgram"): 
                     sgramFile = filename
             print(output)
+            # Return match or no match to client side script
             if (output.find('NOMATCH') != -1):
                 return 'No Match' + ';;'
             else:
--- src/static/scripts/audio_record.js
+++ src/static/scripts/audio_record.js
@@ -1,33 +1,39 @@
 const player = document.querySelector('#player');
- 
+let clockFunction;
+// Function for reading user audio
 navigator.mediaDevices.getUserMedia({ audio: true })
 .then(stream => {
-    mediaRecorder = new MediaRecorder(stream);
-    let audioChunks = []
+    mediaRecorder = new MediaRecorder(stream); 
+    let audioChunks = []    // Store audio chunks
 
+    // Push audio data to audioChunks array
     mediaRecorder.addEventListener('dataavailable', e => {
         audioChunks.push(e.data);
-    });
+    }); 
+    // Create event listener for when audio recording stops
     mediaRecorder.addEventListener("stop", () => {
+        // Create blob from audioChunks
         const audioBlob = new Blob(audioChunks, {type: 'audio/webm;codecs=opus'});
+        // Make POST request to server-side script with audio file
         const req = new XMLHttpRequest;
         req.open('POST', '/audio-upload', true);
+        // Handle return from server-side script post audio analysis
         req.onload = function() {
             console.log(req.response)
-            if (req.response.length > 100) {
-                req.response = 'No Match';
-            }
+            // Reset audioChunks array for next read
             audioChunks = [];
-            songName = document.querySelector('#song-name');
-
+            // Parse the server response
             song = req.response.substring(0, req.response.search(';'));
             sgramFile = req.response.substring(req.response.search(';') + 1, req.response.length);
+            // Set song name in html #song-name element
+            songName = document.querySelector('#song-name');
             songName.textContent = song;
-
+            // Select html elements that need to be modified
             const audioContainer = document.querySelector('#audio-container');
             const sgramContainer = document.querySelector('#sgram-container');
             const audioPlayer = document.querySelector('#audio-player');
             const sgramImage = document.querySelector('#sgram-image');
+            // Handle displaying/hiding elements based on server response
             if (!req.response.includes('No Match')) {
                 audioPlayer.src = '/static/songs/' + encodeURI(song) + '.mp3';
                 sgramImage.src = '/static/' + sgramFile;
@@ -39,11 +45,12 @@ navigator.mediaDevices.getUserMedia({ audio: true })
                 sgramContainer.classList.add('d-none');
             }
         }
+        // Package audio file into form and send to server-side
         let fd = new FormData();
         fd.append("audio_data", audioBlob, "filename.webm");
         req.send(fd);
     });
-
+    // Audio recorder on click event
     player.addEventListener('click', e => {
         const icon = document.querySelector('#record-icon');
         if (player.dataset.action == 'ready') {
@@ -51,10 +58,44 @@ navigator.mediaDevices.getUserMedia({ audio: true })
             icon.classList.replace('oi-microphone', 'oi-media-record');
             icon.classList.remove('large-icon');
             player.dataset.action = 'recording';
+            resetClock();
+            clockFunction = window.setInterval(updateClock, 10);
         } else {
             mediaRecorder.stop();
             icon.classList.replace('oi-media-record', 'oi-microphone');
             player.dataset.action = 'ready';
+            window.clearInterval(clockFunction);
         }
     })
 });
+
+function resetClock() {
+    document.querySelector('#minutes').textContent = '00';
+    document.querySelector('#seconds').textContent = '00';
+    document.querySelector('#deciseconds').textContent = '00';
+}
+// Timer function to update clock
+function updateClock() {
+    minutesElement = document.querySelector('#minutes');
+    secondsElement = document.querySelector('#seconds');
+    decisecondsElement = document.querySelector('#deciseconds');
+
+    deciseconds = parseInt(decisecondsElement.textContent) + 1;
+    // Update time values
+    if (deciseconds >= 100) {
+        decisecondsElement.textContent = '00';
+        secondsText = (parseInt(secondsElement.textContent) + 1).toString();
+        if (secondsText.length < 2) secondsText = '0' + secondsText;
+        secondsElement.textContent = secondsText;
+        if (secondsElement.textContent >= 60) {
+            secondsElement.textContent = '00';
+            minutesText = (parseInt(minutesElement.textContent) + 1).toString();
+            if (minutesText.length < 2) minutesText = '0' + minutesText;
+            minutesElement.textContent = minutesText;
+        }
+    } else {
+        decisecondsText = deciseconds.toString();
+        if (decisecondsText.length < 2) decisecondsText = '0' + decisecondsText;
+        decisecondsElement.textContent = decisecondsText;
+    }
+}
